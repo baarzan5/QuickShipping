f48145a2b6d260d6076da0e0c56998d305d72c10
barzan <barzanzero567@gmail.com>
2024-05-27 09:55:54 -0700

    Add address button to UserAddressModal.jsx

--- src/components/modals/UserAddressModal.jsx
+++ src/components/modals/UserAddressModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { IoCloseOutline } from "react-icons/io5";
+import { IoAddCircleOutline, IoCloseOutline } from "react-icons/io5";
 import { hideScrollBar } from "../../hooks/hideScrollBar";
 import { useLocations } from "../../context/LocationsContext";
 import { useOrders } from "../../context/OrdersContext";
@@ -7,6 +7,8 @@ import { ORDERSACTIONS } from "../../actions/ordersActions";
 import { FormatMoney } from "../../utils/FormatMoney";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConfig";
+import { IoIosAdd } from "react-icons/io";
+import AddAddressModal from "./AddAddressModal";
 
 const UserAddressModal = ({
   showUserAddressModal,
@@ -18,7 +20,7 @@ const UserAddressModal = ({
 }) => {
   hideScrollBar(showUserAddressModal);
 
-  const { address, getUserAddress } = useLocations();
+  const { address, getUserAddress, getCountries, countries } = useLocations();
 
   useEffect(() => {
     if (user) {
@@ -28,6 +30,7 @@ const UserAddressModal = ({
 
   const [activeAddress, setActiveAddress] = useState(null);
   const { handleOrder, dispatch } = useOrders();
+  const [showAddAddressModal, setShowAddAddressModal] = useState(false);
 
   useEffect(() => {
     if (address.length > 0) {
@@ -38,6 +41,10 @@ const UserAddressModal = ({
   const handleAddressClick = (selectedAddress) => {
     setActiveAddress(selectedAddress);
   };
+
+  useEffect(() => {
+    getCountries();
+  }, [countries]);
 
   const handleOrderProduct = async () => {
     try {
@@ -78,67 +85,88 @@ const UserAddressModal = ({
       onClick={() => setShowUserAddressModal(!showUserAddressModal)}
       style={{ zIndex: 999 }}
     >
-      <div
-        className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 w-[90%] h-[300px] overflow-y-auto bg-white rounded-md flex flex-col justify-start items-end gap-2.5 p-2"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="flex justify-between items-center w-full px-2">
-          <span></span>
-          <h3 className="text-lg font-semibold">پشکنین</h3>
+      {!showAddAddressModal ? (
+        <div
+          className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 w-[90%] h-[300px] overflow-y-auto bg-white rounded-md flex flex-col justify-start items-end gap-2.5 p-2"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="flex justify-between items-center w-full px-2">
+            <span></span>
+            <h3 className="text-lg font-semibold">پشکنین</h3>
+            <button
+              title="داخستن"
+              onClick={() => setShowUserAddressModal(!showUserAddressModal)}
+              className="hover:bg-[#969393]/25 rounded-full p-1 active:scale-95 transform transition-all duration-100 ease-in-out"
+            >
+              <IoCloseOutline size={23} />
+            </button>
+          </div>
+
+          <div className="flex flex-row-reverse flex-wrap gap-3 p-1.5">
+            {address.map((address, index) => (
+              <div
+                key={index}
+                className={`cursor-pointer w-[250px] p-2 border rounded-md flex flex-col justify-end items-end gap-2.5 ${
+                  activeAddress === address
+                    ? "border-[#FF6F00]"
+                    : "border-[#e4e4e5]"
+                }`}
+                onClick={() => handleAddressClick(address)}
+              >
+                <div className="flex flex-row-reverse gap-2">
+                  <strong>: وڵات</strong>
+                  <p>{address.country.countryName}</p>
+                </div>
+
+                <div className="flex flex-row-reverse gap-2">
+                  <strong>: شار</strong>
+                  <p>{address.city.cityName}</p>
+                </div>
+
+                <div className="flex flex-row-reverse gap-2">
+                  <strong>: ناونیشان</strong>
+                  <p>{address.address}</p>
+                </div>
+
+                <div className="flex flex-row-reverse gap-2">
+                  <strong>: ژ.مۆبایل</strong>
+                  <p>{address.phoneNumber}</p>
+                </div>
+
+                <div className="flex flex-row-reverse gap-2">
+                  <strong>: نرخی گەیاندن</strong>
+                  <p>{FormatMoney(address.city.deliveryPrice)} IQD</p>
+                </div>
+              </div>
+            ))}
+
+            <button
+              onClick={() => setShowAddAddressModal(!showAddAddressModal)}
+              className="bg-gray-200 cursor-pointer w-[250px] p-2 border rounded-md flex flex-col justify-center items-center gap-2 transform transition-all duration-100 ease-in-out active:scale-95"
+            >
+              <IoIosAdd size={100} />
+              <p className="text-xl">زیادکردنی ناونیشانی نوێ</p>
+            </button>
+          </div>
           <button
-            title="داخستن"
-            onClick={() => setShowUserAddressModal(!showUserAddressModal)}
-            className="hover:bg-[#969393]/25 rounded-full p-1 active:scale-95 transform transition-all duration-100 ease-in-out"
+            onClick={handleOrderProduct}
+            className="bg-[#FF6F00] w-[150px] text-black rounded-md p-2 transform transition-all duration-100 ease-in-out hover:text-white active:scale-95"
           >
-            <IoCloseOutline size={23} />
+            داواکردن
           </button>
         </div>
-
-        <div className="flex flex-row-reverse flex-wrap gap-3 p-1.5">
-          {address.map((address, index) => (
-            <div
-              key={index}
-              className={`cursor-pointer w-[250px] p-2 border rounded-md flex flex-col justify-end items-end gap-2.5 ${
-                activeAddress === address
-                  ? "border-[#FF6F00]"
-                  : "border-[#e4e4e5]"
-              }`}
-              onClick={() => handleAddressClick(address)}
-            >
-              <div className="flex flex-row-reverse gap-2">
-                <strong>: وڵات</strong>
-                <p>{address.country.countryName}</p>
-              </div>
-
-              <div className="flex flex-row-reverse gap-2">
-                <strong>: شار</strong>
-                <p>{address.city.cityName}</p>
-              </div>
-
-              <div className="flex flex-row-reverse gap-2">
-                <strong>: ناونیشان</strong>
-                <p>{address.address}</p>
-              </div>
-
-              <div className="flex flex-row-reverse gap-2">
-                <strong>: ژ.مۆبایل</strong>
-                <p>{address.phoneNumber}</p>
-              </div>
-
-              <div className="flex flex-row-reverse gap-2">
-                <strong>: نرخی گەیاندن</strong>
-                <p>{FormatMoney(address.city.deliveryPrice)} IQD</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <button
-          onClick={handleOrderProduct}
-          className="bg-[#FF6F00] w-[150px] text-black rounded-md p-2 transform transition-all duration-100 ease-in-out hover:text-white active:scale-95"
-        >
-          داواکردن
-        </button>
-      </div>
+      ) : (
+        <>
+          {showAddAddressModal && (
+            <AddAddressModal
+              showAddAddressModal={showAddAddressModal}
+              setShowAddAddressModal={setShowAddAddressModal}
+              userEmail={user.email}
+              countries={countries}
+            />
+          )}
+        </>
+      )}
     </div>
   );
 };
